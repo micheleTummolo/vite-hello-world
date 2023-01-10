a1a90fb2945f48a8ca4c053cc35774a125ab8250
micheleTummolo <tummolomichele02@gmail.com>
2023-01-10 19:31:17 +0100

    Done first app

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    }
+
+}
+</script>
+
+<template lang="">
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<style>
+
+</style>
